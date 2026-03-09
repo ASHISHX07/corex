45497b7b1fbaa3bb777ffe0a2f4d17babc9f512c
Ashish <ashishsindhav02@outlook.com>
2026-03-09 21:46:21 +0530

    tiny change in helpers

--- 01_Gateway_Node/helpers/12hrTime.js
+++
@@ -1,5 +0,0 @@
-export default async function time12Hr() {
-    const now = new Date();
-    const time = now.toLocaleTimeString('en-IN', {hour: 'numeric', minute: 'numeric', second: "numeric", hour12: true});
-    return time;
-}
--- 01_Gateway_Node/helpers/bufferLayouts.js
+++
@@ -1,3 +0,0 @@
-async function float64Buffer(...object) {
-
-}
