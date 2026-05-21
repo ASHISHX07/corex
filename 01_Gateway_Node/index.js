18fec6568778614c3fd0776f7857b00d3d8db1e6
unknown <ashishsindhav02@outlook.com>
2026-05-21 10:40:29 +0530

    minor change ~office

--- 01_Gateway_Node/index.js
+++ 01_Gateway_Node/index.js
@@ -101,9 +101,9 @@ if(!accessToken) {
     console.log("\n[AUTH] Done\n");
 }
 
-optionChainStream(appId, accessToken, "NSE:NIFTY265523800CE", 2);
+optionChainStream(appId, accessToken, "NSE:NIFTY2650523800CE", 1, true);
 
-tbtDataSocket(appId, accessToken, ["NSE:NIFTY265524350CE"], 4);
+// tbtDataSocket(appId, accessToken, ["NSE:NIFTY265524350CE"], 4);
 
 const streamConfig = {
     app_id: appId,
@@ -116,7 +116,7 @@ const streamConfig = {
     logWriter: false
 }
 
-optionAndIndicsStream(streamConfig);
+// optionAndIndicsStream(streamConfig);
 
 console.log("[NODE] Complete");
 controllerBufferView[controllerMap.systemStatus] = 1; // READY!
--- 01_Gateway_Node/streams/api-streams/option-chain.stream.js
+++ 01_Gateway_Node/streams/api-streams/option-chain.stream.js
@@ -6,16 +6,24 @@ import ensureAndMkdir from "../../helpers/ensureAndMkdir.helper.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const logDir = path.join(__dirname, '../../../Data/logs/stream_logs/option-chain-logs');
 
-async function optionChainStream(app_id, access_token, symbol, strikeCount, interval = 4000, logger = false, timeStamp = ``) {
+async function optionChainStream(app_id, access_token, symbol, strikeCount, getGreeks = true, interval = 4000, logger = false, timeStamp = ``) {
     
     const fyers = new fyersModel({"path": ensureAndMkdir(logDir), "enableLogging": logger});
     fyers.setAppId(app_id);
     fyers.setAccessToken(access_token);
 
+    const data = {
+        "symbol": symbol,
+        "strikecount": strikeCount,
+        "timestamp": timeStamp,
+        "greeks": getGreeks ? 1 : undefined
+    }
+
     const poll = async () => {
         try {
-            let optionData = await fyers.getOptionChain({"symbol": symbol, "strikecount": strikeCount, "timestamp": timeStamp, "greeks": 1});
-            console.log(optionData.data);
+            let optionData = await fyers.getOptionChain(data);
+            // console.log(optionData.data);
+            console.log(JSON.stringify(optionData, null, 2));
         }
         catch (err) {
             console.error("Failed Polling in option-chain.stream.js: ", err);
