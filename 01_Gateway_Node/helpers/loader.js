3ff1a71664098e02b4dcb12e517b90adc0ea2db1
Ashish <ashishsindhav02@outlook.com>
2026-08-20 17:55:35 +0530

    inital file/folder structure done

--- 01_Gateway_Node/helpers/loader.js
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { safeRead } from "./fs.helper.js";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-export const config = JSON.parse(safeRead(path.resolve(__dirname, '../../Config/option-config.json')));
--- 02_Strategies_Python/shm/__init__.py
+++
@@ -1,4 +0,0 @@
-from .reader import ShmReader
-from .types import IndexData, OptionData
-
-__all__ = ['ShmReader', 'IndexData', 'OptionData']
