5d5878a6cfba60b266abde06b6dd578f5ae68d4b
Ashish ~office <ashishsindhav02@outlook.com>
2026-08-25 16:19:55 +0530

    add more global usings

--- src/platform/CoreX.Gateway/Usings.cs
+++ src/platform/CoreX.Gateway/Usings.cs
@@ -5,5 +5,12 @@ global using System.Net.Http;
 global using System.Text.Json;
 global using System.Threading;
 global using System.Threading.Tasks;
+global using System.Diagnostics;
+
+// Memory
+global using System.Buffers;
 global using System.Collections.Generic;
+global using System.Collections.Concurrent;
+
+// Third-Party
 global using Microsoft.Extensions.DependencyInjection;
